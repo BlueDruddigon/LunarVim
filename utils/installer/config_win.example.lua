2412f6acf11160a9ac8cee84bd1d0b9fd6d7e657
BlueDruddigon <haibuihoang1999@gmail.com>
2022-07-30 19:55:14 +0700

    Revert "Update plugin LSP"
    This reverts commit 034daa8f362054cd76c70e6c29593de01760c330.

--- utils/installer/config_win.example.lua
+++ utils/installer/config_win.example.lua
@@ -173,7 +173,6 @@ local root_files = {
 }
 
 local opts = {
-  on_attach = lvim.lsp.on_attach_callback,
   root_dir = require("lspconfig.util").root_pattern(unpack(root_files)),
   single_file_support = true,
   filetypes = { "python" },
@@ -213,7 +212,6 @@ local ts_root_files = {
 }
 
 local ts_opts = {
-  on_attach = lvim.lsp.on_attach_callback,
   init_options = { hostInfo = "neovim" },
   root_dir = require("lspconfig.util").root_pattern(unpack(ts_root_files)),
   filetypes = {
@@ -251,8 +249,6 @@ require("lvim.lsp.manager").setup("tailwindcss", {})
 -- end
 lvim.lsp.on_attach_callback = function(_, bufnr)
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  require("lsp_signature").on_attach()
 
   --Enable completion triggered by <c-x><c-o>
   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
